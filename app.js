6eb37c309e96442560c6895211b59be9040d9b63
jooo0922 <2014102458@khu.ac.kr>
2021-02-12 01:53:46 +0900

    upload files

---
+++ app.js
@@ -0,0 +1,150 @@
+'use strict';
+
+import {
+  GlowParticle
+} from './glowparticle.js';
+
+// gradation 효과에 사용할 컬러들을 배열로 정리함.
+const COLORS = [{
+    r: 45,
+    g: 74,
+    b: 227
+  }, // blue
+  {
+    r: 250,
+    g: 255,
+    b: 89
+  }, // yellow
+  {
+    r: 255,
+    g: 104,
+    b: 248
+  }, // pupple
+  {
+    r: 44,
+    g: 209,
+    b: 252
+  }, // skyblue
+  {
+    r: 54,
+    g: 233,
+    b: 84
+  }, // green
+];
+
+class App {
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    document.body.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext('2d');
+
+    this.pixelRatio = (window.devicePixelRatio > 1) ? 2 : 1;
+    /**
+     * Window.devicePixelRatio 읽기 전용 속성은 현재 표시 장치의 물리적 픽셀과 CSS 픽셀의 비율을 반환함.
+     * CSS 픽셀의 크기를 모니터의 물리적 픽셀의 크기로 나눈 값으로 해석해도 됩니다. 
+     * 또 다른 해석은, 하나의 CSS 픽셀을 그릴 때 사용해야 하는 장치 픽셀의 수라고 할 수 있습니다.
+     * 
+     * devicePixelRatio 속성은 HiDPI/Retina 디스플레이처럼 같은 객체를 그릴 때 더 많은 픽셀을 사용해 
+     * 보다 선명한 이미지를 표현하는 화면과, 표준 디스플레이의 렌더링 차이에 대응할 때 유용합니다.
+     * 
+     * 예를 들어, CSS 픽셀 크기가 3mm 정도 된다고 치자. 근데 내 모니터의 픽셀 크기가 2mm야
+     * 그러면 CSS 픽셀 크기 / 내 모니터 물리적 픽셀 크기 = 3 / 2 = 1.5
+     * 또 다른 해석으로는, CSS 픽셀 하나를 그릴려면 내 모니터에서는 픽셀 1.5개가 필요하다는 뜻이기도 하지.
+     * 
+     * 이게 무슨 뜻이냐면, devicePixelRatio에서 return받는 값이 크면 클수록 뭐다?
+     * 사용자 모니터의 픽셀 집적도가 높다는, 즉 고해상도라는 뜻임.
+     * 그래서 해상도가 서로 다른 모니터에서 이 값을 콘솔로 출력해보면 값이 다 다름. 
+     * 
+     * 즉, 'CSS 픽셀 크기 / 사용자마다 모니터에서 지원하는 물리적 픽셀의 크기'의 값이 1보다 크다면, 2를 return하여 pixelRatio에 할당하고,
+     * 1과 같거나 1보다 작다면 1을 return하여 pixelRatio에 할당하라는 뜻.
+     * 
+     * 즉, 값이 크다면 고해상도, 즉 CSS픽셀 개수보다 더 많은 픽셀들로 캔버스를 표현할 수 있으니까
+     * canvas 사이즈 설정 시 브라우저 사이즈에 2를 곱해줘서 레티나 디스플레이를 고려한 사이즈를 설정하고,
+     * 
+     * 값이 작거나 같다면 CSS 픽셀 개수와 같거나, 또는 그보다 적은 개수로 캔버스를 표현할 수 밖에 없으니까
+     * canvas 사이즈 설정 시 굳이 브라우저 사이즈에 2를 곱해줄 필요가 없다는 뜻. 
+     * 그냥 1을 곱해줘서 원래 브라우저 사이즈대로 설정하라는 뜻.
+     */
+
+    this.totalParticles = 15;
+    this.particles = [];
+    this.maxRadius = 900;
+    this.minRadius = 400;
+
+    window.addEventListener('resize', this.resize.bind(this), false);
+    // // false로 지정하면 이벤트 버블링, true로 지정하면 이벤트 캡쳐링이 발생함.
+    this.resize();
+
+    window.requestAnimationFrame(this.animate.bind(this)); // 인스턴스 생성되자마자 애니메이션 바로 걸어서 호출해주고
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
+    // 이번에는 그냥 레티나 디스플레이 고려해서 무조건 * 2를 한 것이 아니라,
+    // devicePixelRatio 속성에서 return받은 값을 이용하여, 사용자의 모니터 환경에 따라
+    // 사용자의 모니터가 레티나를 지원할만큼 높은 해상도를 지원하면 1를 곱하고, 그렇지 않으면 2를 곱하도록 한 것.
+
+    /**
+     * CanvasRenderingContext2D.globalCompositeOperation = 'type'
+     * 새로 그리는 도형은 언제나 이미 그려진 도형의 위에 그려졌음.
+     * 대부분의 상황에서는 이렇게 하는 것이 적절하지만, 만약 도형끼리 합성(compostion)효과를 내고 싶다면?
+     * 
+     * globalCompositeOperation = type
+     * 이걸 이용해서 새로운 도형을 그릴 때, 도형 합성 방법을 설정함. 
+     * type 자리에는 26가지의 합성 방법을 지정해줄 수 있음.
+     * 
+     * saturation은 아래쪽 레이어의 색상과 명도를 보존하고 위쪽 레이어의 채도를 적용함.
+     * 이거는 사실 그냥 MDN에 나와있는 type 26개 참고해서 
+     * 다 지정해보면서 원하는 효과가 적용되는 걸로 조정하는 게 제일 나을거 같음. 
+     */
+    this.ctx.globalCompositeOperation = 'saturation';
+
+    this.creatParticles();
+  }
+
+  creatParticles() {
+    let curColor = 0;
+    this.particles = [];
+
+    for (let i = 0; i < this.totalParticles; i++) {
+      const item = new GlowParticle(
+        Math.random() * this.stageWidth,
+        Math.random() * this.stageHeight, // GlowParticle의 x, y 좌표값은 stage의 width와 height 내에서 각각 무작위로 지정될 것.
+        Math.random() * (this.maxRadius - this.minRadius) + this.minRadius, // GlowParticle의 반지름값은 40 ~ 90 사이의 숫자들 중에서 무작위로 지정될 것. (랜덤값 범위 구하는 공식 알지?)
+        COLORS[curColor]
+      );
+
+      if (++curColor >= COLORS.length) { // if 조건문 내에서 바로 curColor값, 즉 COLORS의 index로 접근할 때 쓰일 값 증가연산 함.
+        curColor = 0; // 만약 그 값이 COLORS의 배열 개수(5개) 보다 같아지거나 커지면 다시 0번 index로 reset하라는 거.  
+      }
+      // 그니까 결과적으로는 COLORS 안에 있는 5개의 색깔들로만 GlowParticle들이 생성되겠지?
+
+      this.particles[i] = item; // 이거는 for loop 돌때마다 해당 index에 생성된 GlowParticle 인스턴스를 집어넣으란 거지? 
+    }
+  }
+
+  animate() {
+    window.requestAnimationFrame(this.animate.bind(this)); // 내부에서 스스로를 반복 호출해주고
+
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight); // 이전 프레임은 항상 지워주고
+
+    // totalParticles에 지정한 개수만큼 for loop로 반복해서 GlowParticle의 애니메이션을 그려주는 거
+    for (let i = 0; i < this.totalParticles; i++) {
+      const item = this.particles[i];
+      // 여기서 사용된 const item은 createParticles()의 item과 관련이 없음. global scope에서 정의한 게 아니라면 이름을 똑같이 써도 상관 없음.
+
+      item.animate(this.ctx, this.stageWidth, this.stageHeight);
+      // 매 프레임을 그릴때마다 totalParticles에 지정된 갯수만큼 GlowParticle들을 그려주는 데,
+      // 이때 각각의 GlowParticle들 마다 x, y좌표값과 반지름값을 바꿔서 실제 캔버스에 그려줌!
+    }
+  }
+}
+
+window.onload = () => {
+  new App();
+};
---
+++ glowparticle.js
@@ -0,0 +1,107 @@
+'use strict';
+
+const PI2 = Math.PI * 2; // 360도 값을 radian 단위로 표현함. 원을 만들 때 쓰려고 한거겠지?
+
+export class GlowParticle {
+  constructor(x, y, radius, rgb) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.rgb = rgb;
+
+    this.vx = Math.random() * 4;
+    this.vy = Math.random() * 4; // 0 ~ 4 사이의 난수를 return받음. x, y좌표값에 더해주려는 속도값인 것 같음.
+
+    this.sinValue = Math.random(); // 0 ~ 1 사이의 난수를 sinValue에 할당
+  }
+
+  animate(ctx, stageWidth, stageHeight) {
+    this.sinValue += 0.01; // 이 함수를 호출할 때마다 sinValue는 0.01씩 늘어남
+
+    this.radius += Math.sin(this.sinValue);
+    // 예를 들어서 0.01 rad, 0.02 rad, 0.03 rad, 이런 식으로 0.01씩 늘어나는 radian값을 전달해서 
+    // -1 ~ 1 사이의 sin값을 return받고, 그걸 radius(즉, 반지름값)에 더해서 넣어줌.
+    // 결과적으로는 매 프레임마다 1 ~ -1 사이에서 늘어났다 줄어났다 하는 값을 반지름값에 누산해주겠지?
+    // 0더해주고 그 다음에는 0.1 더해주고 그 다음에는 0.2, 0.3 ... 이런 식으로 쭉쭉 나가겠지? 그냥 예를 들자면 이런 거.
+
+    this.x += this.vx;
+    this.y += this.vy;
+    // 0 ~ 1 사이로 return받은 난수값을 할당받은 속도값(vx,vy)을 계속 더해주면서 위치가 이동하겠지?
+
+    if (this.x < 0) {
+      this.vx *= -1; // x 좌표값(원의 중심점이) stage의 왼쪽 끝을 벗어나려고 할 경우 vx만큼 이동하는 '방향'을 바꿔줌
+      this.x += 10; // 방향을 바꿔줌과 동시에 x좌표값을 10으로 옮겨줌. 약간 튕겨지는 느낌. 갑자기 한 번에 10만큼 이동하니까.
+    } else if (this.x > stageWidth) {
+      this.vx *= -1; // y 좌표값(원의 중심점이)이 stage 오른쪽 끝을 벗어나려고 할 때 '이동 방향'을 또 바꿔 줌.
+      this.x -= 10; // 동시에 x좌표값을 stageWidth - 10으로 옮기려는 것. 
+    }
+
+    // y 좌표값도 마찬가지로 해주기
+    if (this.y < 0) {
+      this.vy *= -1;
+      this.y += 10;
+    } else if (this.y > stageHeight) {
+      this.vy *= -1;
+      this.y -= 10;
+    }
+    // 이렇게 하면 전체적으로 원의 중심점이 스크린에 닿을 때마다 10씩 튕겨짐과 함께 방향을 바꿔서 움직이는 모습이겠지?
+
+    ctx.beginPath();
+
+    /**
+     * CanvasRenderingContext2D.createRadialGradient() 
+     * 이 메서드는 두 원의 크기와 좌표를 사용하여 반지름 상에 그라데이션을 만듦.
+     * 
+     * 이 메서드는 Canvas Gradient를 반환하며,
+     * 도형에 적용하려면 먼저 fillStyle 또는 strokeStyle 속성에 그라데이션을 할당해줘야 함.
+     * 
+     * 이 그라데이션을 할당해주려고 쓰는 메소드가 CanvasGradient.addColorStop()임.
+     * 이 메소드를 호출할때는 그라디언트가 create된 객체에서 호출해야 함.
+     * 그래서 아래에서 g.addColorStop으로 그라디언트가 생성된 g에서 호출한거임.
+     */
+    const g = ctx.createRadialGradient(
+      this.x,
+      this.y,
+      this.radius * 0.01,
+      this.x,
+      this.y,
+      this.radius
+    );
+    // 안에 들어가는 파라미터들을 간단하게 좀 설명해주자면,
+    // 앞에 3개는 그라데이션이 시작하는 원인 시작 원의 x, y 좌표값(즉, 중심점)과 반지름
+    // 뒤의 3개는 그라데이션이 끝나는 원인 끝 원의 x, y좌표값(즉, 중심점)과 반지름을 의미함.
+    // 우리는 현재 캔버스 상에 그려진 원을 기준으로, 같은 중심점에 위치하지만 반지름이 0.01배인 아주 작은 원에서
+    // 현재 캔버스 상에 그려진 원까지 그라데이션을 주려고 한 것이기 때문에 
+    // 중심점은 같으면서, 
+
+    /**
+     * CanvasGradient.addColorStop()
+     * 이 메소드는 주어진 canvas gradient에서 
+     * '어떤 지점에서부터 어떤 컬러로 시작해서' '어떤 지점까지 어떤 컬러로 끝나는'
+     * 일종의 그라데이션 상의 color stop을 새롭게 추가해 주는거.
+     * 포토샵에서 그라데이션 줄 때 그라데이션 색상표 위에다가 점찍는 거 있지? 그 점을 colorStop에 해당함.
+     * 
+     * gradient.addColorStop(offset, color); 요런 문법을 사용하는데,
+     * offset은 0 ~ 1사이의 숫자로 color stop의 위치를 지정해 줌. 
+     * 0은 그라데이션의 시작점. 1은 그라데이션의 끝점
+     * 
+     * color는 당연히 그 지점에 어떤 컬러로 color stop을 찍을 것인가에 해당함. CSS 컬러값을 넣으면 됨.
+     * 여기서는 시작원은 0 지점에서 같은 컬러인데 투명도가 1인 색깔로 그라데이션을 시작해서
+     * 끝원은 1 지점에서 같은 컬러인데 투명도가 0인 색깔로 그라데이션을 끝냄. 
+     * 그냥 투명도만 그라데이션을 조절해준 거임.
+     */
+    g.addColorStop(0, `rgba(${this.rgb.r}, ${this.rgb.g}, ${this.rgb.b}, 1)`);
+    g.addColorStop(1, `rgba(${this.rgb.r}, ${this.rgb.g}, ${this.rgb.b}, 0)`)
+
+    // 시작원과 끝원을 지정해서 두 원의 반지름 상에 그라데이션을 만들고,
+    // 어떤 지점 어떤 컬러에서 어떤 지점 어떤 컬러까지 그라에이션을 줄 것인지 colorStop을 추가하면,
+    // 그라데이션을 만들어냈기 때문에 이렇게 fillStyle에 할당하기만 하면 됨. 
+    ctx.fillStyle = g;
+    // ctx.fillStyle = `rgba(${this.rgb.r}, ${this.rgb.g}, ${this.rgb.b}, 1)`; // app.js의 COLOR 배열의 item들을 전달받아서 할당하는거겠지?
+
+    ctx.arc(this.x, this.y, this.radius, 0, PI2, false)
+    ctx.fill();
+  }
+  // 원의 전체적인 움직임은 원의 중심점이 스크린 끝에 맞닿을 때마다 10씩 튕겨지고 
+  // 이동방향을 반대로 바꿔서 움직임과 동시에 반지름은 커졌다 작아졌다 하는 모습일거임.
+}
